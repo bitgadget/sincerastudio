42f0961a7996cd10ab3c3750105b252023270d4f
BITGADGET <bitgadgetshop@gmail.com>
2025-05-10 19:03:24 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from './components/Hero';
+import Workflow from './components/Workflow';
+
+export default function App() {
+  return (
+    <>
+      <Hero />
+      <Workflow />
+    </>
+  );
+}
